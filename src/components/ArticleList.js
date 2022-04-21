a0cafea6c95f4ff444de99f593c866ca8dbdcfb0
costamay <titusouko@gmail.com>
2022-04-21 12:17:49 +0300

    Completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}){
+    const articleItem = posts.map((article) =>{
+      return  < Article 
+        key = {article.id}
+        title = {article.title}
+        date = {article.date}
+        preview = {article.preview}/>
+});
+    return(
+        <main>
+            {articleItem}
+        </main>
+    );
+}
+
+export default ArticleList;
